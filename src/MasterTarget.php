dbe83b45639baa0fa2f102c0ea75d154e584c089
Milind Kagdelwar <milind.kagdelwar@acquia.com>
2021-04-11 12:50:23 +0530

    Added target for master.

---
+++ src/MasterTarget.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drutiny\BlazeMeter;
+
+use Drutiny\Container;
+use Drutiny\Target\DrushTarget;
+use Drutiny\Target\InvalidTargetException;
+use Drutiny\Driver\DrushRouter;
+
+/**
+ * @Drutiny\Annotation\Target(
+ *  name = "blazemter-master"
+ * )
+ */
+class MasterTarget extends DrushTarget {
+
+  protected $master;
+
+  public function validate()
+  {
+    $drush = DrushRouter::createFromTarget($this);
+    $drush->setOptions([
+      'format' => 'int',
+    ]);
+    $status = $drush->status();
+
+    if (!isset($status['files'])) {
+      throw new InvalidTargetException("Drush status indicates target is not valid: " . $this->uri());
+    }
+    return parent::validate();
+  }
+
+}
+
+
+ ?>
